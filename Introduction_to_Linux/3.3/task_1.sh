e848c3c85267072fb6415571d5cb70798395b478
Alferov Kirill <alferovkirill.mai@yandex.ru>
2024-02-24 23:43:53 +0300

    Branches and loops

--- Introduction_to_Linux/3.3/task_1.sh
+++ Introduction_to_Linux/3.3/task_1.sh
@@ -2,19 +2,19 @@
 
 if [[ $1 -eq 0 ]]
 then
-	echo "No students"
+    echo "No students"
 elif [[ $1 -eq 1 ]]
 then
-	echo "1 student"
+    echo "1 student"
 elif [[ $1 -eq 2 ]]
 then
-	echo "2 students"
+    echo "2 students"
 elif [[ $1 -eq 3 ]]
 then
-	echo "3 students"
+    echo "3 students"
 elif [[ $1 -eq 4 ]]
 then
-	echo "4 students"
+    echo "4 students"
 else
-	echo "A lot of students"
+    echo "A lot of students"
 fi
--- Introduction_to_Linux/3.3/task_2.sh
+++ Introduction_to_Linux/3.3/task_2.sh
@@ -2,31 +2,31 @@
 
 while [[ true ]]
 do
-	echo "enter your name:"
-	read name
+    echo "enter your name:"
+    read name
 
-	if [[ -z $name ]]
-	then
-		echo "bye"
-		break
-	fi
+    if [[ -z $name ]]
+    then
+        echo "bye"
+        break
+    fi
 
-	echo "enter your age:"
-	read age
+    echo "enter your age:"
+    read age
 
-	if [[ age -eq 0 ]]
-	then
-		echo "bye"
-		break
-	fi
+    if [[ age -eq 0 ]]
+    then
+        echo "bye"
+        break
+    fi
 
-	if [[ age -le 16 ]]
-	then
-		echo "${name}, your group is child"
-	elif [[ age -ge 17 && age -le 25 ]]
-	then
-		echo "${name}, your group is youth"
-	else
-	echo "${name}, your group is adult"
-	fi
+    if [[ age -le 16 ]]
+    then
+        echo "${name}, your group is child"
+    elif [[ age -ge 17 && age -le 25 ]]
+    then
+        echo "${name}, your group is youth"
+    else
+    echo "${name}, your group is adult"
+    fi
 done
