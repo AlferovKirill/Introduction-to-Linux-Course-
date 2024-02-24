61a3df612a094daf1111d12b22f1b827fefa6799
Alferov Kirill <alferovkirill.mai@yandex.ru>
2024-02-24 23:44:37 +0300

    Different

--- Introduction_to_Linux/3.4/task_2.sh
+++ Introduction_to_Linux/3.4/task_2.sh
@@ -2,20 +2,20 @@
 
 while [[ true ]]
 do
-	read a exp b
+    read a exp b
 
-	if [[ "$a" == "exit" ]]
-	then
-		echo "bye"
-		exit
-	fi
+    if [[ "$a" == "exit" ]]
+    then
+        echo "bye"
+        exit
+    fi
 
-	if [[ $exp == "+" || $exp == "-" || $exp == "*" || $exp == "/" || $exp == "%" || $exp == "**" ]]
-	then
-		let "result=$a $exp $b"
-		echo "$result"
-	else
-		echo "error"
-		exit	
-	fi
+    if [[ $exp == "+" || $exp == "-" || $exp == "*" || $exp == "/" || $exp == "%" || $exp == "**" ]]
+    then
+        let "result=$a $exp $b"
+        echo "$result"
+    else
+        echo "error"
+        exit    
+    fi
 done
