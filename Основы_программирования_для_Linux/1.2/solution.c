cd288490ff8ddc992fca15bf12d25f7622b3a8dd
Alferov Kirill <alferovkirill.mai@yandex.ru>
2024-03-23 21:00:33 +0300

    1.2

--- Основы_программирования_для_Linux/1.2/solution.c
+++ Основы_программирования_для_Linux/1.2/solution.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stddef.h>
 
-int stringStat(const char *string, size_t multiplier, int *count) {
+int stringStat(const char* string, size_t multiplier, int* count) {
     ++(*count);
     return strlen(string) * multiplier;
 }
